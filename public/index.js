c82b37225d59c096533ad743d070c7a9d1dc5f1f
Jean BaMaung <jbamaung17@gmail.com>
2018-04-16 01:36:36 +0100

    fixed it so it shows all pages of the API and not just the first one, am nearly 100% sure there was a much easier way of doing that

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+var locations;
 var characters;
 var episodes;
 
@@ -63,12 +64,15 @@ const populateCharactersSelector = function(characters){
 // create listed item
 const showCharacterInfo = function(event){
   const indexNumber = event.target.selectedIndex;
-  var pTag = document.querySelector('#character-info');
-  pTag.innerText = "this is working"
+  var characterDiv = document.querySelector('#character-div');
+  
 }
 
 
-// create character image
+// create character item
+
+
+
 
 
 
@@ -111,13 +115,68 @@ const showLocationInfo = function(event){
 
 //run app
 const app = function(){
-  const url1 = "https://rickandmortyapi.com/api/character/";
-  const url2 = "https://rickandmortyapi.com/api/episode/";
-  const url3 = "https://rickandmortyapi.com/api/location/";
+  const url1 = "https://rickandmortyapi.com/api/character/?page=1";
+  const url11 = "https://rickandmortyapi.com/api/character/?page=2";
+  const url12 = "https://rickandmortyapi.com/api/character/?page=3";
+  const url13 = "https://rickandmortyapi.com/api/character/?page=4";
+  const url14 = "https://rickandmortyapi.com/api/character/?page=5";
+  const url15 = "https://rickandmortyapi.com/api/character/?page=6";
+  const url16 = "https://rickandmortyapi.com/api/character/?page=7";
+  const url17 = "https://rickandmortyapi.com/api/character/?page=8";
+  const url18 = "https://rickandmortyapi.com/api/character/?page=9";
+  const url19 = "https://rickandmortyapi.com/api/character/?page=10";
+  const url101 = "https://rickandmortyapi.com/api/character/?page=11";
+  const url102 = "https://rickandmortyapi.com/api/character/?page=12";
+  const url103 = "https://rickandmortyapi.com/api/character/?page=13";
+  const url104 = "https://rickandmortyapi.com/api/character/?page=14";
+  const url105 = "https://rickandmortyapi.com/api/character/?page=15";
+  const url106 = "https://rickandmortyapi.com/api/character/?page=16";
+  const url107 = "https://rickandmortyapi.com/api/character/?page=17";
+  const url108 = "https://rickandmortyapi.com/api/character/?page=18";
+  const url109 = "https://rickandmortyapi.com/api/character/?page=19";
+  const url110 = "https://rickandmortyapi.com/api/character/?page20";
+
+  const url2 = "https://rickandmortyapi.com/api/episode/?page=1";
+  const url21 = "https://rickandmortyapi.com/api/episode/?page=2";
+
+  const url3 = "https://rickandmortyapi.com/api/location/?page=1";
+  const url31 = "https://rickandmortyapi.com/api/location/?page=2";
+  const url32 = "https://rickandmortyapi.com/api/location/?page=3";
+  const url33 = "https://rickandmortyapi.com/api/location/?page=4";
 
 makeRequest(url1, characterRequestComplete);
+makeRequest(url11, characterRequestComplete);
+makeRequest(url12, characterRequestComplete);
+makeRequest(url13, characterRequestComplete);
+makeRequest(url14, characterRequestComplete);
+makeRequest(url15, characterRequestComplete);
+makeRequest(url16, characterRequestComplete);
+makeRequest(url17, characterRequestComplete);
+makeRequest(url18, characterRequestComplete);
+makeRequest(url19, characterRequestComplete);
+makeRequest(url101, characterRequestComplete);
+makeRequest(url102, characterRequestComplete);
+makeRequest(url103, characterRequestComplete);
+makeRequest(url104, characterRequestComplete);
+makeRequest(url105, characterRequestComplete);
+makeRequest(url106, characterRequestComplete);
+makeRequest(url107, characterRequestComplete);
+makeRequest(url108, characterRequestComplete);
+makeRequest(url109, characterRequestComplete);
+makeRequest(url110, characterRequestComplete);
+
 makeRequest(url2, episodeRequestComplete);
-makeRequest(url3, locationRequestComplete)
+makeRequest(url21, episodeRequestComplete);
+
+
+
+makeRequest(url3, locationRequestComplete);
+makeRequest(url31, locationRequestComplete);
+makeRequest(url32, locationRequestComplete);
+makeRequest(url33, locationRequestComplete);
+
+
+
 
 
 const select = document.getElementById('#selectcharacter');
